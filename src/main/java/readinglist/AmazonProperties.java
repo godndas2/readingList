b4434bae5a1574b4d3b072dc7935aaba3334c0e0
halfdev <gusgodnd@naver.com>
2019-11-15 00:18:34 +0900

    ref #5 logging, amazonID hyperlink

--- src/main/java/readinglist/AmazonProperties.java
+++ src/main/java/readinglist/AmazonProperties.java
@@ -1,19 +1,21 @@
 package readinglist;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+/**
+* @author halfdev
+* @since 2019-11-14
+* Amazon 에 주요 구성 Property
+*/
 @Component
 @ConfigurationProperties("amazon")
+@Getter
+@Setter
 public class AmazonProperties {
 
     private String associateId;
 
-    public void setAssociateId(String associateId) {
-        this.associateId = associateId;
-    }
-
-    public String getAssociateId() {
-        return associateId;
-    }
 }
--- src/main/java/readinglist/ReadingListController.java
+++ src/main/java/readinglist/ReadingListController.java
@@ -1,6 +1,7 @@
 package readinglist;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
